527740145daad1aa7900d063a3ee8fff830f8391
bsy1141 <bsy1141@naver.com>
2023-01-14 15:44:04 +0900

    sass 설치 후 next.config.js 설정 변경

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
+const path = require("path");
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  sassOptions: {
+    includePaths: [path.join(__dirname, "styles")], // 2. sassOptions 옵션 추가
+    prependData: `@import "styles/_variables.scss"; @import "styles/_mixins.scss";`,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
