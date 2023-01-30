305e62df0aafe71079be639a82d76e8797e89301
bsy1141 <bsy1141@naver.com>
2023-01-31 00:20:52 +0900

    feat: 검색 메인페이지 뷰 작업

--- pages/search.jsx
+++ pages/search.jsx
@@ -1,7 +1,59 @@
+import { useRouter } from "next/router";
+
+import BookCard from "./component/bookCard";
+import data from "@/public/data/recommend.json";
+import styles from "@/styles/Search.module.scss";
+
+// NOTE: 임시 추천 검색어 데이터
+const keywords = ["주식", "영어", "고전", "인테리어", "다이어트", "IT/인터넷"];
+
+const SearchInput = () => {
+  return (
+    <div className={styles.input_container}>
+      <div className={styles.input_wrapper}>
+        <input className={styles.input} placeholder="검색어를 입력해주세요" />
+        <img src="/images/search.svg" alt="search icon" />
+      </div>
+    </div>
+  );
+};
+
+const PopularKeywords = () => {
+  return (
+    <>
+      <p className={styles.subTitle}>추천 검색어</p>
+      <div className={styles.keyword_wrapper}>
+        {keywords.map((keyword) => (
+          <div className={styles.keyword}>{keyword}</div>
+        ))}
+      </div>
+    </>
+  );
+};
+
+const RecommendBook = () => {
+  return (
+    <div className={styles.recommend}>
+      <p className={styles.subTitle}>추천 책</p>
+      {data.book.map((book) => (
+        <BookCard book={book} isSearchCard />
+      ))}
+    </div>
+  );
+};
+
 function Search() {
+  const router = useRouter();
+
   return (
     <div>
-      <div>검색창</div>
+      <button onClick={() => router.back()} className={styles.backButton}>
+        <img src="/images/backButton.svg" alt="back button" />
+      </button>
+      <div className={styles.title}>검색</div>
+      <SearchInput />
+      <PopularKeywords />
+      <RecommendBook />
     </div>
   );
 }
