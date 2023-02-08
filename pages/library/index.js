a7baea35288e01a74f4749029e0d2e57915ed1b4
Minji Kim <sml07113@gmail.com>
2023-02-08 21:50:57 +0900

    feat: 내서재 탭 API 연동

--- pages/library/index.js
+++ pages/library/index.js
@@ -1,160 +1,22 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useRouter } from "next/router";
+import axios from "axios";
 
 import styles from "@/styles/Library.module.scss";
 import Button from "@/components/common/Button";
 import BookCard from "@/components/common/bookCard";
 import Navigation from "@/components/common/Navigation";
 
-// 더미 데이터
-const bookList = [
+const statusArr = [
   {
-    id: 0,
-    title:
-      "NestJS로 배우는 백엔드 프로그래밍 (타입스크립트 환경의 차세대 서버 프레임워크를 만나다)",
-    author: "한용재",
-    link: "https://search.shopping.naver.com/book/catalog/36250090622",
-    image:
-      "https://shopping-phinf.pstatic.net/main_3625009/36250090622.20221213164928.jpg",
-    discount: "24300",
-    publisher: "제이펍",
-    isbn: "9791192469560",
-    pubdate: "20221220",
-    description:
-      "가장 진보한 프레임워크로 우아하게 백엔드 입문하기\n\nNestJS는 Express를 기반으로 DB, ORM, 유효성 검사 등의 라이브러리를 내장한 차세대 백엔드 프레임워크다. 제어 반전, 의존성 주입, AOP 등 객체 지향 개념을 도입했고 타입스크립트로 모듈/컴포넌트 기반의 프로그래밍을 할 수 있다. 이 책은 저자가 모두싸인에 몸담으며 익힌 노하우와 인사이트를 바탕으로 NestJS를 활용해 회원 가입, 이메일 인증, 로그인, 회원 정보 조회 기능을 갖춘 REST API 유저 서비스를 만들어본다. 프로젝트에 점차 살을 붙여나가며 백엔드 개발에 필요한 환경 변수 설정, 요청 유효성 검사, 인증(JWT), 로깅, 헬스 체크, CQRS, 클린 아키텍처, 단위 테스트 등 지식과 기술을 제대로 익힐 수 있게 집필했다.",
-    memo: 15,
+    title: "읽는 중",
+    isReading: true,
   },
   {
-    id: 1,
-    title:
-      "NestJS로 배우는 백엔드 프로그래밍 (타입스크립트 환경의 차세대 서버 프레임워크를 만나다)",
-    author: "한용재",
-    link: "https://search.shopping.naver.com/book/catalog/36250090622",
-    image:
-      "https://shopping-phinf.pstatic.net/main_3625009/36250090622.20221213164928.jpg",
-    discount: "24300",
-    publisher: "제이펍",
-    isbn: "9791192469560",
-    pubdate: "20221220",
-    description:
-      "가장 진보한 프레임워크로 우아하게 백엔드 입문하기\n\nNestJS는 Express를 기반으로 DB, ORM, 유효성 검사 등의 라이브러리를 내장한 차세대 백엔드 프레임워크다. 제어 반전, 의존성 주입, AOP 등 객체 지향 개념을 도입했고 타입스크립트로 모듈/컴포넌트 기반의 프로그래밍을 할 수 있다. 이 책은 저자가 모두싸인에 몸담으며 익힌 노하우와 인사이트를 바탕으로 NestJS를 활용해 회원 가입, 이메일 인증, 로그인, 회원 정보 조회 기능을 갖춘 REST API 유저 서비스를 만들어본다. 프로젝트에 점차 살을 붙여나가며 백엔드 개발에 필요한 환경 변수 설정, 요청 유효성 검사, 인증(JWT), 로깅, 헬스 체크, CQRS, 클린 아키텍처, 단위 테스트 등 지식과 기술을 제대로 익힐 수 있게 집필했다.",
-    memo: 15,
-  },
-  {
-    id: 2,
-    title:
-      "NestJS로 배우는 백엔드 프로그래밍 (타입스크립트 환경의 차세대 서버 프레임워크를 만나다)",
-    author: "한용재",
-    link: "https://search.shopping.naver.com/book/catalog/36250090622",
-    image:
-      "https://shopping-phinf.pstatic.net/main_3625009/36250090622.20221213164928.jpg",
-    discount: "24300",
-    publisher: "제이펍",
-    isbn: "9791192469560",
-    pubdate: "20221220",
-    description:
-      "가장 진보한 프레임워크로 우아하게 백엔드 입문하기\n\nNestJS는 Express를 기반으로 DB, ORM, 유효성 검사 등의 라이브러리를 내장한 차세대 백엔드 프레임워크다. 제어 반전, 의존성 주입, AOP 등 객체 지향 개념을 도입했고 타입스크립트로 모듈/컴포넌트 기반의 프로그래밍을 할 수 있다. 이 책은 저자가 모두싸인에 몸담으며 익힌 노하우와 인사이트를 바탕으로 NestJS를 활용해 회원 가입, 이메일 인증, 로그인, 회원 정보 조회 기능을 갖춘 REST API 유저 서비스를 만들어본다. 프로젝트에 점차 살을 붙여나가며 백엔드 개발에 필요한 환경 변수 설정, 요청 유효성 검사, 인증(JWT), 로깅, 헬스 체크, CQRS, 클린 아키텍처, 단위 테스트 등 지식과 기술을 제대로 익힐 수 있게 집필했다.",
-    memo: 15,
-  },
-  {
-    id: 3,
-    title:
-      "NestJS로 배우는 백엔드 프로그래밍 (타입스크립트 환경의 차세대 서버 프레임워크를 만나다)",
-    author: "한용재",
-    link: "https://search.shopping.naver.com/book/catalog/36250090622",
-    image:
-      "https://shopping-phinf.pstatic.net/main_3625009/36250090622.20221213164928.jpg",
-    discount: "24300",
-    publisher: "제이펍",
-    isbn: "9791192469560",
-    pubdate: "20221220",
-    description:
-      "가장 진보한 프레임워크로 우아하게 백엔드 입문하기\n\nNestJS는 Express를 기반으로 DB, ORM, 유효성 검사 등의 라이브러리를 내장한 차세대 백엔드 프레임워크다. 제어 반전, 의존성 주입, AOP 등 객체 지향 개념을 도입했고 타입스크립트로 모듈/컴포넌트 기반의 프로그래밍을 할 수 있다. 이 책은 저자가 모두싸인에 몸담으며 익힌 노하우와 인사이트를 바탕으로 NestJS를 활용해 회원 가입, 이메일 인증, 로그인, 회원 정보 조회 기능을 갖춘 REST API 유저 서비스를 만들어본다. 프로젝트에 점차 살을 붙여나가며 백엔드 개발에 필요한 환경 변수 설정, 요청 유효성 검사, 인증(JWT), 로깅, 헬스 체크, CQRS, 클린 아키텍처, 단위 테스트 등 지식과 기술을 제대로 익힐 수 있게 집필했다.",
-    memo: 15,
-  },
-  {
-    id: 4,
-    title:
-      "NestJS로 배우는 백엔드 프로그래밍 (타입스크립트 환경의 차세대 서버 프레임워크를 만나다)",
-    author: "한용재",
-    link: "https://search.shopping.naver.com/book/catalog/36250090622",
-    image:
-      "https://shopping-phinf.pstatic.net/main_3625009/36250090622.20221213164928.jpg",
-    discount: "24300",
-    publisher: "제이펍",
-    isbn: "9791192469560",
-    pubdate: "20221220",
-    description:
-      "가장 진보한 프레임워크로 우아하게 백엔드 입문하기\n\nNestJS는 Express를 기반으로 DB, ORM, 유효성 검사 등의 라이브러리를 내장한 차세대 백엔드 프레임워크다. 제어 반전, 의존성 주입, AOP 등 객체 지향 개념을 도입했고 타입스크립트로 모듈/컴포넌트 기반의 프로그래밍을 할 수 있다. 이 책은 저자가 모두싸인에 몸담으며 익힌 노하우와 인사이트를 바탕으로 NestJS를 활용해 회원 가입, 이메일 인증, 로그인, 회원 정보 조회 기능을 갖춘 REST API 유저 서비스를 만들어본다. 프로젝트에 점차 살을 붙여나가며 백엔드 개발에 필요한 환경 변수 설정, 요청 유효성 검사, 인증(JWT), 로깅, 헬스 체크, CQRS, 클린 아키텍처, 단위 테스트 등 지식과 기술을 제대로 익힐 수 있게 집필했다.",
-    memo: 15,
-  },
-  {
-    id: 5,
-    title:
-      "NestJS로 배우는 백엔드 프로그래밍 (타입스크립트 환경의 차세대 서버 프레임워크를 만나다)",
-    author: "한용재",
-    link: "https://search.shopping.naver.com/book/catalog/36250090622",
-    image:
-      "https://shopping-phinf.pstatic.net/main_3625009/36250090622.20221213164928.jpg",
-    discount: "24300",
-    publisher: "제이펍",
-    isbn: "9791192469560",
-    pubdate: "20221220",
-    description:
-      "가장 진보한 프레임워크로 우아하게 백엔드 입문하기\n\nNestJS는 Express를 기반으로 DB, ORM, 유효성 검사 등의 라이브러리를 내장한 차세대 백엔드 프레임워크다. 제어 반전, 의존성 주입, AOP 등 객체 지향 개념을 도입했고 타입스크립트로 모듈/컴포넌트 기반의 프로그래밍을 할 수 있다. 이 책은 저자가 모두싸인에 몸담으며 익힌 노하우와 인사이트를 바탕으로 NestJS를 활용해 회원 가입, 이메일 인증, 로그인, 회원 정보 조회 기능을 갖춘 REST API 유저 서비스를 만들어본다. 프로젝트에 점차 살을 붙여나가며 백엔드 개발에 필요한 환경 변수 설정, 요청 유효성 검사, 인증(JWT), 로깅, 헬스 체크, CQRS, 클린 아키텍처, 단위 테스트 등 지식과 기술을 제대로 익힐 수 있게 집필했다.",
-    memo: 15,
-  },
-  {
-    id: 6,
-    title:
-      "NestJS로 배우는 백엔드 프로그래밍 (타입스크립트 환경의 차세대 서버 프레임워크를 만나다)",
-    author: "한용재",
-    link: "https://search.shopping.naver.com/book/catalog/36250090622",
-    image:
-      "https://shopping-phinf.pstatic.net/main_3625009/36250090622.20221213164928.jpg",
-    discount: "24300",
-    publisher: "제이펍",
-    isbn: "9791192469560",
-    pubdate: "20221220",
-    description:
-      "가장 진보한 프레임워크로 우아하게 백엔드 입문하기\n\nNestJS는 Express를 기반으로 DB, ORM, 유효성 검사 등의 라이브러리를 내장한 차세대 백엔드 프레임워크다. 제어 반전, 의존성 주입, AOP 등 객체 지향 개념을 도입했고 타입스크립트로 모듈/컴포넌트 기반의 프로그래밍을 할 수 있다. 이 책은 저자가 모두싸인에 몸담으며 익힌 노하우와 인사이트를 바탕으로 NestJS를 활용해 회원 가입, 이메일 인증, 로그인, 회원 정보 조회 기능을 갖춘 REST API 유저 서비스를 만들어본다. 프로젝트에 점차 살을 붙여나가며 백엔드 개발에 필요한 환경 변수 설정, 요청 유효성 검사, 인증(JWT), 로깅, 헬스 체크, CQRS, 클린 아키텍처, 단위 테스트 등 지식과 기술을 제대로 익힐 수 있게 집필했다.",
-    memo: 15,
-  },
-  {
-    id: 7,
-    title:
-      "NestJS로 배우는 백엔드 프로그래밍 (타입스크립트 환경의 차세대 서버 프레임워크를 만나다)",
-    author: "한용재",
-    link: "https://search.shopping.naver.com/book/catalog/36250090622",
-    image:
-      "https://shopping-phinf.pstatic.net/main_3625009/36250090622.20221213164928.jpg",
-    discount: "24300",
-    publisher: "제이펍",
-    isbn: "9791192469560",
-    pubdate: "20221220",
-    description:
-      "가장 진보한 프레임워크로 우아하게 백엔드 입문하기\n\nNestJS는 Express를 기반으로 DB, ORM, 유효성 검사 등의 라이브러리를 내장한 차세대 백엔드 프레임워크다. 제어 반전, 의존성 주입, AOP 등 객체 지향 개념을 도입했고 타입스크립트로 모듈/컴포넌트 기반의 프로그래밍을 할 수 있다. 이 책은 저자가 모두싸인에 몸담으며 익힌 노하우와 인사이트를 바탕으로 NestJS를 활용해 회원 가입, 이메일 인증, 로그인, 회원 정보 조회 기능을 갖춘 REST API 유저 서비스를 만들어본다. 프로젝트에 점차 살을 붙여나가며 백엔드 개발에 필요한 환경 변수 설정, 요청 유효성 검사, 인증(JWT), 로깅, 헬스 체크, CQRS, 클린 아키텍처, 단위 테스트 등 지식과 기술을 제대로 익힐 수 있게 집필했다.",
-    memo: 15,
-  },
-  {
-    id: 8,
-    title:
-      "NestJS로 배우는 백엔드 프로그래밍 (타입스크립트 환경의 차세대 서버 프레임워크를 만나다)",
-    author: "한용재",
-    link: "https://search.shopping.naver.com/book/catalog/36250090622",
-    image:
-      "https://shopping-phinf.pstatic.net/main_3625009/36250090622.20221213164928.jpg",
-    discount: "24300",
-    publisher: "제이펍",
-    isbn: "9791192469560",
-    pubdate: "20221220",
-    description:
-      "가장 진보한 프레임워크로 우아하게 백엔드 입문하기\n\nNestJS는 Express를 기반으로 DB, ORM, 유효성 검사 등의 라이브러리를 내장한 차세대 백엔드 프레임워크다. 제어 반전, 의존성 주입, AOP 등 객체 지향 개념을 도입했고 타입스크립트로 모듈/컴포넌트 기반의 프로그래밍을 할 수 있다. 이 책은 저자가 모두싸인에 몸담으며 익힌 노하우와 인사이트를 바탕으로 NestJS를 활용해 회원 가입, 이메일 인증, 로그인, 회원 정보 조회 기능을 갖춘 REST API 유저 서비스를 만들어본다. 프로젝트에 점차 살을 붙여나가며 백엔드 개발에 필요한 환경 변수 설정, 요청 유효성 검사, 인증(JWT), 로깅, 헬스 체크, CQRS, 클린 아키텍처, 단위 테스트 등 지식과 기술을 제대로 익힐 수 있게 집필했다.",
-    memo: 15,
+    title: "완독",
+    isReading: false,
   },
 ];
-
-const statusArr = ["읽는 중", "완독"];
 
 const NoBookList = () => {
   const router = useRouter();
@@ -168,7 +30,7 @@ const NoBookList = () => {
         radius="12px"
         padding="12px 24px"
         children="책 고르러가기"
-        onClick={() => router.push("/search")}
+        onClick={() => router.push("/home/search")}
       />
     </div>
   );
@@ -177,7 +39,25 @@ const NoBookList = () => {
 function Library() {
   const router = useRouter();
   const [activeIndex, setActiveIndex] = useState(0);
+  const [bookList, setBookList] = useState([]);
   const handleStatusClick = (index) => setActiveIndex(index);
+
+  const getBookList = async () => {
+    const res = await axios.get(
+      `${process.env.NEXT_PUBLIC_ENDPOINT}/books?isReading=${statusArr[activeIndex].isReading}`,
+      {
+        headers: {
+          //todo: localstorage에 저장된 토큰으로 변경
+          Authorization: `Bearer ${localStorage.getItem("userToken")}`,
+        },
+      },
+    );
+    setBookList(res.data.books);
+  };
+
+  useEffect(() => {
+    getBookList();
+  }, [activeIndex]);
 
   return (
     <div>
@@ -192,7 +72,7 @@ function Library() {
                 : styles.status_btn_deSelected
             }
           >
-            {status}
+            {status.title}
           </div>
         ))}
       </div>
@@ -205,9 +85,17 @@ function Library() {
             <div className={styles.library_container}>
               {bookList.map((book) => (
                 <BookCard
-                  key={book.id}
+                  key={book.ID}
                   book={book}
-                  handleClick={() => router.push(`/memo/${book.id}`)}
+                  handleClick={() =>
+                    router.push(
+                      {
+                        pathname: `/library/memo/${book.ID}`,
+                        query: { bookId: book.ID },
+                      },
+                      `/library/memo/${book.ID}`,
+                    )
+                  }
                   isLibraryCard
                 />
               ))}
--- pages/library/memo/[id].jsx
+++ pages/library/memo/[id].jsx
@@ -1,107 +1,10 @@
 import { useEffect, useState } from "react";
 import { useRouter } from "next/router";
+import axios from "axios";
 
 import styles from "@/styles/Memo.module.scss";
 import Button from "@/components/common/Button";
 import MemoCard from "@/components/common/MemoCard";
-
-// 더미 데이터
-const bookDetail = {
-  title:
-    "NestJS로 배우는 백엔드 프로그래밍 (타입스크립트 환경의 차세대 서버 프레임워크를 만나다)",
-  author: "한용재",
-  link: "https://search.shopping.naver.com/book/catalog/36250090622",
-  image:
-    "https://shopping-phinf.pstatic.net/main_3625009/36250090622.20221213164928.jpg",
-  discount: "24300",
-  publisher: "제이펍",
-  isbn: "9791192469560",
-  pubdate: "20221220",
-  description:
-    "가장 진보한 프레임워크로 우아하게 백엔드 입문하기\n\nNestJS는 Express를 기반으로 DB, ORM, 유효성 검사 등의 라이브러리를 내장한 차세대 백엔드 프레임워크다. 제어 반전, 의존성 주입, AOP 등 객체 지향 개념을 도입했고 타입스크립트로 모듈/컴포넌트 기반의 프로그래밍을 할 수 있다. 이 책은 저자가 모두싸인에 몸담으며 익힌 노하우와 인사이트를 바탕으로 NestJS를 활용해 회원 가입, 이메일 인증, 로그인, 회원 정보 조회 기능을 갖춘 REST API 유저 서비스를 만들어본다. 프로젝트에 점차 살을 붙여나가며 백엔드 개발에 필요한 환경 변수 설정, 요청 유효성 검사, 인증(JWT), 로깅, 헬스 체크, CQRS, 클린 아키텍처, 단위 테스트 등 지식과 기술을 제대로 익힐 수 있게 집필했다.",
-  memo: 15,
-};
-
-const memoList = [
-  {
-    bookId: 0,
-    category: "느낀점",
-    createdAt: "string",
-    deletedAt: {
-      time: "string",
-      valid: true,
-    },
-    id: 0,
-    text: "Lorem ipsum dolor sit amet consectetur. Phasellus nec dui dolor mattis malesuada. Tortor justo platea cursus amet mi magnis id. At nisi sed sed eros risus. Quam amet viverra duis ultricies adipiscing neque maecenas donec.",
-    updatedAt: "string",
-    userId: 0,
-  },
-  {
-    bookId: 0,
-    category: "배운점",
-    createdAt: "string",
-    deletedAt: {
-      time: "string",
-      valid: true,
-    },
-    id: 1,
-    text: "Lorem ipsum dolor sit amet consectetur. Phasellus nec dui dolor mattis malesuada. Tortor justo platea cursus amet mi magnis id. At nisi sed sed eros risus. Quam amet viverra duis ultricies adipiscing neque maecenas donec.",
-    updatedAt: "string",
-    userId: 0,
-  },
-  {
-    bookId: 0,
-    category: "구절",
-    createdAt: "string",
-    deletedAt: {
-      time: "string",
-      valid: true,
-    },
-    id: 2,
-    text: "Lorem ipsum dolor sit amet consectetur. Phasellus nec dui dolor mattis malesuada. Tortor justo platea cursus amet mi magnis id. At nisi sed sed eros risus. Quam amet viverra duis ultricies adipiscing neque maecenas donec.",
-    updatedAt: "string",
-    userId: 0,
-  },
-  {
-    bookId: 0,
-    category: "구절",
-    createdAt: "string",
-    deletedAt: {
-      time: "string",
-      valid: true,
-    },
-    id: 3,
-    text: "Lorem ipsum dolor sit amet consectetur. Phasellus nec dui dolor mattis malesuada. Tortor justo platea cursus amet mi magnis id. At nisi sed sed eros risus. Quam amet viverra duis ultricies adipiscing neque maecenas donec.",
-    updatedAt: "string",
-    userId: 0,
-  },
-  {
-    bookId: 0,
-    category: "구절",
-    createdAt: "string",
-    deletedAt: {
-      time: "string",
-      valid: true,
-    },
-    id: 4,
-    text: "Lorem ipsum dolor sit amet consectetur. Phasellus nec dui dolor mattis malesuada. Tortor justo platea cursus amet mi magnis id. At nisi sed sed eros risus. Quam amet viverra duis ultricies adipiscing neque maecenas donec.",
-    updatedAt: "string",
-    userId: 0,
-  },
-  {
-    bookId: 0,
-    category: "구절",
-    createdAt: "string",
-    deletedAt: {
-      time: "string",
-      valid: true,
-    },
-    id: 5,
-    text: "Lorem ipsum dolor sit amet consectetur. Phasellus nec dui dolor mattis malesuada. Tortor justo platea cursus amet mi magnis id. At nisi sed sed eros risus. Quam amet viverra duis ultricies adipiscing neque maecenas donec.",
-    updatedAt: "string",
-    userId: 0,
-  },
-];
 
 const tagArr = ["전체", "느낀점", "배운점", "구절"];
 
@@ -210,14 +113,34 @@ const BookInfo = ({ title, author, publisher, image }) => {
 
 function Memo() {
   const router = useRouter();
+  const [bookDetail, setBookDetail] = useState([]);
+  const [memoList, setMemoList] = useState([]);
   const [activeTag, setActiveTag] = useState(0);
   const [isModalSmallVisible, setModalSmallVisible] = useState(false);
   const [isModalBookVisible, setModalBookVisible] = useState(false);
   const [showPopUp, setShowPopUp] = useState(router.query.isEdited || false);
-  const [isReading, setIsReading] = useState(true);
   const handleTagClick = (index) => setActiveTag(index);
   const handleModalSmall = () => setModalSmallVisible(!isModalSmallVisible);
   const handleModalBook = () => setModalBookVisible(!isModalBookVisible);
+
+  const getMemoList = async () => {
+    const res = await axios.get(
+      `${process.env.NEXT_PUBLIC_ENDPOINT}/books/${router.query.bookId}`,
+      {
+        headers: {
+          //todo: localstorage에 저장된 토큰으로 변경
+          Authorization: `Bearer ${localStorage.getItem("userToken")}`,
+        },
+      },
+    );
+    setBookDetail(res.data);
+    setMemoList(res.data.memos);
+  };
+
+  useEffect(() => {
+    getMemoList();
+  }, []);
+
   useEffect(() => {
     if (showPopUp) {
       setTimeout(() => setShowPopUp(false), 3000);
@@ -247,17 +170,17 @@ function Memo() {
       <div className={styles.division}></div>
       <div className={styles.record_date_container}>
         <div className={styles.subtitle}>독서 기록</div>
-        {!isReading && (
+        {!bookDetail.isReading && (
           <div className={styles.record_complete}>🎉완독했어요!🎉</div>
         )}
         <div className={styles.date_container}>
           <div className={styles.date_title}>시작한 날짜</div>
-          <div className={styles.date}>YYYY.MM.DD</div>
+          <div className={styles.date}>{bookDetail.CreatedAt}</div>
         </div>
         {memoList.length !== 0 && (
           <div className={styles.date_container}>
             <div className={styles.date_title}>마지막 날짜</div>
-            <div className={styles.date}>2023.01.28</div>
+            <div className={styles.date}>{bookDetail.UpdatedAt}</div>
           </div>
         )}
       </div>
@@ -286,20 +209,20 @@ function Memo() {
             <div className={styles.memo_list_count}>{memoList.length}개</div>
             {memoList.map((memo) => (
               <MemoCard
-                key={memo.id}
+                key={memo.ID}
                 memo={memo}
                 handleClick={() =>
                   router.push(
                     {
                       pathname: "/record",
                       query: {
-                        id: memo.id,
+                        id: memo.ID,
                         isEditPage: true,
                         memoText: memo.text,
                         memoTag: memo.category,
                       },
                     },
-                    `/edit?memoId=${memo.id}`,
+                    `/edit?memoId=${memo.ID}`,
                   )
                 }
               />
