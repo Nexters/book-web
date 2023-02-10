9b24a42d849d29832953a378a9181c36c6889e9c
bsy1141 <bsy1141@naver.com>
2023-02-10 20:59:11 +0900

    feat: 파비콘, 오픈그래프 이미지 추가

--- pages/_document.js
+++ pages/_document.js
@@ -4,6 +4,13 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
+        <meta
+          property="og:title"
+          content="세상에서 제일 쉬운 독서기록, 책조각"
+        />
+        <meta property="og:url" content="https://pieceofbook.com" />
+        <meta property="og:image" content="/images/og-image.png" />
+
         <link
           href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"
           rel="stylesheet"
