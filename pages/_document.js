bdf1284b9704450b2eb1c979415c3a29bdcba9c4
bsy1141 <bsy1141@naver.com>
2023-02-13 17:27:15 +0900

    fix: 검색 페이지 텍스트 색깔 지정 & 다크모드 해제 처리

--- pages/_document.js
+++ pages/_document.js
@@ -11,6 +11,10 @@ export default function Document() {
         />
         <meta property="og:url" content="https://pieceofbook.com" />
         <meta property="og:image" content="/images/og-image.png" />
+        <head>
+          <meta name="color-scheme" content="light only" />
+          <meta name="supported-color-schemes" content="light" />
+        </head>
         <link
           href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"
           rel="stylesheet"
